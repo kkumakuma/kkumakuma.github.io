76f2f9563afb50518c55e9a1b04d565a9faf1edd
kkumakuma <62420542+kkumakuma@users.noreply.github.com>
2020-08-19 03:15:17 +1000

    fix layout

--- resources/collapsiblenav.js
+++ resources/collapsiblenav.js
@@ -1,15 +1,26 @@
-function togNav() {
+function toggleNav() {
   var nav = document.getElementById("sidebar");
   var navcont = document.getElementById("sidenavcontent")
-  if (nav.style.width == '20em') {
-    nav.style.width = '6em';
+  var bodycont = document.getElementById("main")
+  var navbutton = document.getElementById("navbtnicon")
+
+  if (nav.style.width == '6em') {
+    nav.style.width = '20em';
+    nav.style.backgroundColor = '#f7aab3';
+    nav.style.boxShadow = '5px 10px 5px #d4abc1';
     nav.style.opacity = 1;
-    navcont.style.visibility = "hidden";
-    document.getElementById("main").style.marginLeft = "0";
+
+    navbutton.style.color = 'white';
+    navcont.style.visibility = 'visible';
+    bodycont.style.marginLeft = '24em';
   } else {
-    nav.style.width = "20em";
+    nav.style.width = '6em';
+    nav.style.backgroundColor = 'white';
+    nav.style.boxShadow = 'none';
     nav.style.opacity = 1;
-    navcont.style.visibility = "visible";
-    document.getElementById("main").style.marginLeft = "6em";
+
+    navbutton.style.color = '#f7aab3'
+    navcont.style.visibility = 'hidden';
+    bodycont.style.marginLeft = '10em';
   }
 }
