f91bf2d065a35f022dd46bc7b5f86738ca4a1925
kkumakuma <insidiouspaenda@gmail.com>
2020-08-19 14:12:00 +1000

    sidebar toggle

--- resources/collapsiblenav.js
+++ resources/collapsiblenav.js
@@ -1,38 +1,57 @@
-function toggleNav() {
-  var nav = document.getElementById("sidebar");
-  var navcont = document.getElementById("sidenavcontent")
-  var bodycont = document.getElementById("main")
-  var navbutton = document.getElementById("navbtnicon")
+/*sessionStorage.clear();*/
+var nav = document.getElementById("sidebar");
+var navcont = document.getElementById("sidenavcontent");
+var bodycont = document.getElementById("main");
+var navbutton = document.getElementById("navbtnicon");
 
-  if (nav.style.width == '6em') {
-    nav.style.width = '20em';
-    nav.style.backgroundColor = '#f7aab3';
-    nav.style.boxShadow = '5px 10px 5px #d4abc1';
-    nav.style.opacity = 1;
-
-    navbutton.style.color = 'white';
-    navcont.style.visibility = 'visible';
-    bodycont.style.marginLeft = '24em';
-  } else {
-    nav.style.width = '6em';
-    nav.style.backgroundColor = 'white';
-    nav.style.boxShadow = 'none';
-    nav.style.opacity = 1;
-
-    navbutton.style.color = '#f7aab3'
-    navcont.style.visibility = 'hidden';
-    bodycont.style.marginLeft = '10em';
-  }
+sideNavStatus = sessionStorage.getItem('sideNavStatus');
+if (sideNavStatus == null) {
+  sideNavStatus = 'on';
+}
+if (sideNavStatus == 'on') {
+  navOn();
+} else if (sideNavStatus == 'off') {
+  navOff();
+} else {
+  navOn();
 }
 
-function chngNvBtn(button_id)
-{
-   if (document.getElementById('navbtnicon').text == '\u{2715}')
-   {
-       document.getElementById('navbtnicon').text = '\u{2630}';
-   }
-   else
-   {
-     document.getElementById('navbtnicon').text = '\u{2715}';
-   }
+function navOn() {
+  nav.style.width = '20em';
+  nav.style.backgroundColor = '#f7aab3';
+  nav.style.boxShadow = '5px 10px 5px #d4abc1';
+  nav.style.opacity = 1;
+
+  navbutton.style.color = 'white';
+  navcont.style.visibility = 'visible';
+  bodycont.style.marginLeft = '24em';
+
+  navbutton.text = '\u{2715}';
+
+  sessionStorage.setItem("sideNavStatus", 'on');
+  sideNavStatus = sessionStorage.getItem('sideNavStatus');
+}
+
+function navOff() {
+  nav.style.width = '6em';
+  nav.style.backgroundColor = 'white';
+  nav.style.boxShadow = 'none';
+  nav.style.opacity = 1;
+
+  navbutton.style.color = '#f7aab3';
+  navcont.style.visibility = 'hidden';
+  bodycont.style.marginLeft = '10em';
+
+  navbutton.text = '\u{2630}';
+
+  sessionStorage.setItem("sideNavStatus", 'off');
+  sideNavStatus = sessionStorage.getItem('sideNavStatus');
+}
+
+function loadNavStatus() {
+  if (sideNavStatus == 'on') {
+    navOff();
+  } else if (sideNavStatus == 'off') {
+    navOn();
+  }
 }
